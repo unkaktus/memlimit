10abddf324e79784263d279d676f1696e0c75fb2
Ivan Markin <sw@nogoegst.net>
2018-10-16 21:56:40 +0000

    Refactor naming to be standard

--- memlimit_linux.go
+++ memlimit_linux.go
@@ -1,9 +1,10 @@
-// memlimit.go - set memory rlimit to cgroup memory limit
+// memlimit_linux.go - set memory rlimit to cgroup memory limit
 //
 // To the extent possible under law, Ivan Markin waived all copyright
 // and related or neighboring rights to this module of memlimit, using the creative
 // commons "CC0" public domain dedication. See LICENSE or
 // <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
+
 // +build linux
 
 package memlimit
